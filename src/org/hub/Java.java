799db1698c8ae29a90b37280ca4f5e51da41e703
Subashkumar24 <subashkumarsk007@gmail.com>
2022-08-10 20:13:43 +0530

    new code added

---
+++ src/org/hub/Java.java
@@ -0,0 +1,23 @@
+package org.hub;
+
+public class Java {
+	
+	public static void main(String[] args) {
+		System.out.println("java 1");
+		
+	
+	}
+	private void Java2() {
+		System.out.println("java 2");
+	}
+	private void java3() {
+		System.out.println("java 3");
+	}
+		
+		
+		
+		
+		
+	}
+
+
